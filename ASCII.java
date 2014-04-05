48ec8f1eac67d8777f9063794ffbc3f02280d163
yalk <ankur2893@gmail.com>
2014-04-05 15:50:20 +0530

    second commit, added comments

--- ASCII.java
+++ ASCII.java
@@ -10,17 +10,17 @@ class ASCII
 		int i=0;
 		while(i!=inputString.length())
 		{
-			positionArray[i]=characterPosition(inputString.charAt(i));
+			positionArray[i]=characterPosition(inputString.charAt(i));		//feeding initial index of every character in the array which will be later used to refer character replacements with ASCII art in the file ASCII
 			i++;
 		}
 
 		for(i=0;i<5;i++)
 		{
-			String temp=br.readLine();
+			String temp=br.readLine();										//temp file, holds a line of the ASCII file, NOTE: readLine(), jumps to the next line after it is run
 			for(int j=0;j<inputString.length();j++)
-			{
+			{																//printing substring of the whole first line
 				System.out.print(temp.substring(positionArray[j],positionArray[j]+5)+" ");
-			}
+			}																//length of each ascii art=5 characters
 			System.out.println();
 		}
 
@@ -28,8 +28,8 @@ class ASCII
 
 	static int characterPosition(char character)
 	{
-		if(character==' ')
+		if(character==' ')													//special case for space
 			return 130;
-		return ((int)character-(int)'a')*5;
+		return ((int)character-(int)'a')*5;									//returns index of the character relative to a
 	}
 }
